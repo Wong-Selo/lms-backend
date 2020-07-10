baf0ab0143044604f5f2935eb6e5891bd5ca8e01
Fizi Kurniawan <fizikurniawan@gmail.com>
2020-07-10 23:21:20 +0700

    fix response error

--- api/app.js
+++ api/app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(generalMiddleware)
 
+app.get('/', (req, res) => res.send('Welcome to LMS API'))
 app.group('/api', (router) => {
   apiRouter(router)
 })
@@ -34,13 +35,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.sendError(err, err.message, err.status)
 });
 
 module.exports = app;
